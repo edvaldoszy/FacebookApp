08a6b1e093a720fa67bdbf5f2919ae37356e5a7b
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-04-10 15:35:49 -0300

    testes dos scripts

--- index.php
+++ index.php
@@ -15,14 +15,28 @@ if (!$app->checkAccess($token)) {
     exit("<a href=\"{$app->getLoginUrl()}\">Fazer login com Facebook</a>");
 }
 
+$link = new Link("http://www.edvaldotsi.com/");
+$post = new Post("Testando aplicativo de postagens automáticas no Facebook");
+$post->setLink($link);
+
 /*
 $profile = new Profile();
-var_dump($app->getPages($profile));
+$pages = $app->getPages($profile);
+var_dump($pages);
+*/
+/*
+foreach ($pages as $page) {
+    $page->publish($post);
+    echo $post->getId();
+}
 */
 
-
+/*
 $profile = new Profile();
 var_dump($app->getGroups($profile));
+*/
+
+var_dump($app->getProfile());
 
 /*
 $link = new Link("http://www.edvaldotsi.com/");
