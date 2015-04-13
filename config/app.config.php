67ed056e979dfb8ab7314f8f0923a9c2b39ad822
Edvaldo Szymonek <edvaldoszy@gmail.com>
2015-04-13 10:06:24 -0300

    config file

---
+++ config/app.config.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    "app_id" => "433974716778616",
+    "app_secret" => "124f17ab103b3af0b041db138ed02dce",
+    "redirect_url" => "http://facebook-app.localhost/logado.php",
+
+    "scope" => array(
+        "publish_actions",
+        "manage_pages",
+        "user_groups"
+    )
+);
